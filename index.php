9976773c668ec079bc89f9ce8756f6ccffa8f590
Edgar Olivera <se17@outlook.com>
2017-10-16 11:40:36 -0500

    First

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <!--meta name="viewport" content="width=device-width, initial-scale=1.0"!-->
+    <meta name="description" content="git, github">
+    <link rel="stylesheet" href="estilo.css">
+        <title>Practica 2. Utilizando git</title>
+        <!-- change !-->
+</head>
+<section>
+    <div class="contenedor">
+        <<form action="#" method="post" name="f1" id="f1">
+            <input type="text" name="v_user"  id="v_user" placeholder="USUARIO">
+            <input type="pasword" name="v_pass" id="v_pass" placeholder="PASSWORD">
+            <input type="submmit" name="Enviar">
+
+        </form>
+    </div>
+</section>
+<body>
+        <div class=cuadro>Test</div>
+        <div class=cuadro2>Test</div>
+        <div class=cuadro3>Test</div>
+        <div class=cuadro4>Test</div>
+</body>
+</html>
